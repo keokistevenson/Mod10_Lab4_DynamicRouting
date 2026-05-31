139da027967393205d5d904eaab8fa4a8886898b
keokistevenson <keokistevenson@gmail.com>
2026-05-31 18:04:32 -0400

    Added code for Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main>
+      <h1>Blog App</h1>
+      <p>This lab demonstrates dynamic routing and protected routes.</p>
+    </main>
+  );
+}
+
+export default Home;
