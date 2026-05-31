09f2f2ec615a24a04959aeff65183adb8053ded4
keokistevenson <keokistevenson@gmail.com>
2026-05-31 18:11:13 -0400

    Added code for App.tsx and Admin.tsx

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,11 @@
+function Admin() {
+  return (
+    <main>
+      <h1>Admin Dashboard</h1>
+      <p>Welcome to the Admin Dashboard.</p>
+      <p>Only logged-in users can see this page.</p>
+    </main>
+  );
+}
+
+export default Admin;
